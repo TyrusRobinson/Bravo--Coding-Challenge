cbf51381d7374a4795caff388c9ee61d332a2318
Tyrus Robinson <110849099+TyrusRobinson@users.noreply.github.com>
2022-12-02 17:40:42 -0600

    Add files via upload

---
+++ C#- Payments/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using Sabio.Models.Domain.Friends;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Payment Get(int id);
+    }
+}
